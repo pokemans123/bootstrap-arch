c7a401273b482fe358631ac41af26890b0f844a0
pokemans123 <pranavarunkumar2010@gmail.com>
2026-05-15 16:04:19 -0400

    updated script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#Meant to be run in chroot!!!
 
 if [ $(whoami) != 'root' ]; then  
   echo 'Please run as root'
@@ -7,17 +6,9 @@ if [ $(whoami) != 'root' ]; then
 fi
 
 echo 'setting up pkglist and logging hooks...'
-mkdir -p /var/lib/pkglist
-touch /var/lib/pkglist/packages.txt
-chmod 644 /var/lib/pkglist/packages.txt
-
-cp ./log-packages.sh /usr/local/bin/log-packages.sh
-chmod +x /usr/local/bin/log-packages.sh
-cp ./log-packages.hook /usr/share/libalpm/hooks/log-packages.hook
-
-cp ./packages.txt /var/lib/pkglist/packages.txt
-
-pkgs=/var/lib/pkglist/packages.txt
-echo 'setup complete!'
+pkgdir=/var/lib/pkglist
+mkdir -p $pkgdir
+ln -sf ./packages.txt $pkgdir/packages.txt
+echo 'setup complete! Please remove the .git file but do not remove this entire directory!'
 
 
