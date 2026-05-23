9bebc2ffb6fe8016865b8c4c5da9df3c0f00e2aa
pokemans123 <pranavarunkumar2010@gmail.com>
2026-05-23 14:22:20 -0400

    added script warnings

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $(whoami) != 'root' ]; then  
+if [ $(whoami) != 'root' ]; then
   echo 'Please run as root'
   exit 0
 fi
@@ -10,5 +10,4 @@ pkgdir=/var/lib/pkglist
 mkdir -p $pkgdir
 ln -sf ./packages.txt $pkgdir/packages.txt
 echo 'setup complete! Please remove the .git file but do not remove this entire directory!'
-
-
+echo 'Beware when installing packages; some may require AUR or extra/lib32 repos to be enabled. To be safe, enable all repositories before installing packages!'
